8659d45be1cd32b318fcdec262f75617d0851436
sravan <sravanpthankappan@gmail.com>
2022-09-01 16:54:14 +0530

    server error

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ app.use('/admin',adminRouter )
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   // next(createError(404));
-  res.status(400).render('error')
+  // res.status(400).render('error')
 });
 
 // error handler
